a72637014ee89367950995e3a8cd147d3c51d704
walefy lima <walefylima@gmail.com>
2025-05-09 00:22:37 -0300

    fix(project): fixed (👤 user: walefy lima)

---
+++ app.config.js
@@ -0,0 +1,23 @@
+import 'dotenv/config';
+
+export default {
+    expo: {
+        name: "test expo",
+        slug: "test-expo",
+        extra: {
+            apiKey: process.env.EXPO_PUBLIC_APPKEY,
+            authDomain: process.env.EXPO_PUBLIC_AUTHDOMAIN,
+            projectId: process.env.EXPO_PUBLIC_PROJECTID,
+            storageBucket: process.env.EXPO_PUBLIC_STORAGEBUCKET,
+            messagingSenderId: process.env.EXPO_PUBLIC_MESSAGINGSENDERID,
+            appId: process.env.EXPO_PUBLIC_APPID,
+            webClientId: process.env.EXPO_PUBLIC_WEBCLIENTID,
+            androidClientId: process.env.EXPO_PUBLIC_ANDROIDCLIENT_ID,
+            redirectUri: process.env.EXPO_PUBLIC_REDIRECTURI,
+            eas: {
+                projectId: "12e5ef74-76b4-4dd2-91f4-c0f7bbec3df8"
+            }
+        },
+        runtimeVersion: "1.0.0"
+    },
+};
